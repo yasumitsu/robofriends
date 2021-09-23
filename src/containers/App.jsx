a51efd4230f59745df24fc44138976ea6a2ae537
Kenji <kenjiyasumitsu@gmail.com>
2021-09-23 14:38:24 +0900

    structure folders

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
+import CardList from '../components/CardList';
 // import { robots } from './robots';
-import SearchBox from './SearchBox';
+import SearchBox from '../components/SearchBox';
 import './App.css';
-import Scroll from './Scroll';
+import Scroll from '../components/Scroll';
 
 class App extends Component {
 	constructor() {
@@ -25,10 +25,9 @@ class App extends Component {
 	};
 
 	render() {
-		const filteredRobot = this.state.robots.filter((robot) =>
-			robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-		);
-		if (this.state.robots.length === 0) return <h1 className="tc">Loading...</h1>;
+		const { robots, searchfield } = this.state;
+		const filteredRobot = robots.filter((robot) => robot.name.toLowerCase().includes(searchfield.toLowerCase()));
+		if (!robots.length) return <h1 className="tc">Loading...</h1>;
 		else {
 			return (
 				<div className="tc">
